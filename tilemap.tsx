0cbf5b0c1d32f7a70f36ed8257b2f33f88c1349b
Lenkwolf <puggychien@gmail.com>
2021-09-03 15:07:02 +1200

    m

--- tilemap.tsx
+++ tilemap.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tilemap" tilewidth="16" tileheight="16" tilecount="64" columns="8">
- <image source="tilemap.png" width="128" height="128"/>
+<tileset version="1.5" tiledversion="1.7.2" name="tilemap" tilewidth="160" tileheight="160" tilecount="64" columns="8">
+ <image source="tilemap.png" width="1280" height="1280"/>
 </tileset>
